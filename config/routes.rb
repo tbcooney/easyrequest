d37f6eb60aa99289628d9a643c6cd9b9cbd869c6
tbcooney <taylor.cooney@vegastudios.ca>
2017-10-11 19:59:40 -0400

    Update routes to root application to devise sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'requests#index'
+
+  authenticated :user do
+    root 'requests#index'
+  end
 
   resources :team_members
   resources :requests
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 end
