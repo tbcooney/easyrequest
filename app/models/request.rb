ea769b0fd78f4cec063927dd8891f34b2d70d437
tbcooney <taylor.cooney@vegastudios.ca>
2017-10-11 22:32:50 -0400

    set_rate in request model once member is assigned

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,13 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :team_member
+
+  # Callbacks
+  before_validation :set_rate
+
+protected
+
+  def set_rate
+    self.rate = team_member.rate
+  end
 end
