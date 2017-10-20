1a870d4e15646098275eb03e20bbf9402d290925
tbcooney <taylor.cooney@vegastudios.ca>
2017-10-20 00:15:44 -0400

    Added migration to add name field to User Devise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012000314) do
+ActiveRecord::Schema.define(version: 20171020040910) do
 
   create_table "requests", force: :cascade do |t|
     t.string   "description"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171012000314) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
